fe93425d2be41551147adc05d0fd0fa16a7f4e6f
KateePankratevaa <124819002+KateePankratevaa@users.noreply.github.com>
2023-05-17 16:15:17 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection.Emit;
+
+namespace Practice15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != String.Empty || textBox2.Text != String.Empty || textBox3.Text != String.Empty)
+                {
+                    Class1 ykaz = new Class1();
+                    int num_of_page = Convert.ToInt32(textBox3.Text);                    
+                    if (num_of_page <= 10 && num_of_page >= 1)
+                    {
+                        string text = textBox1.Text + " " + textBox2.Text;
+                        StreamWriter sw = File.AppendText("file.txt");
+                        sw.WriteLine(text);
+                        MessageBox.Show("Записано в файл", "Успешно", MessageBoxButtons.OK);                      
+                        listBox1.Items.Add(text);
+                        sw.Close();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Номер страницы должен быть от одного до десяти", "Ошибка", MessageBoxButtons.OK);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("У вас есть незаполненные поля", "Ошибка", MessageBoxButtons.OK);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!Данные введены неверно!");
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice15
+{
+    class Class1
+    {
+        private string ykazatel;
+        private string word;
+        private int num_of_page;
+
+        public string Ykazatel { get { return ykazatel; } set { ykazatel = value; } }
+        public string Word { get { return word; } set { word = value; } }
+        public int Num_of_page { get { return num_of_page; } set { num_of_page = value; } }
+
+
+        public void Zad6(string ykazatel, int num_of_page, string word)
+        {
+            this.ykazatel = ykazatel;
+            this.num_of_page = num_of_page;
+            this.word = word;
+        }
+
+        public string Info()
+        {
+            return "Указатель:" + ykazatel + "Заданное слово: " + word + "Номер страниц/ы: " + num_of_page;
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
